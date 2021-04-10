74c9880840759d5c8f6f3aa9e14b2600a8f9671b
Riccardo <riccardo@null.net>
2021-04-10 11:01:20 +0200

    add meme engine and font

---
+++ src/MemeEngine/MemeGenerator.py
@@ -0,0 +1,32 @@
+from PIL import Image, ImageDraw, ImageFont
+from random import randint
+
+class MemeGenerator:
+    def __init__(self, output_dir):
+        self.output_dir = output_dir
+
+    def make_meme(self, img_path, text, author, width=500) -> str:
+        """Create a meme With a Text
+
+        Arguments:
+            img_path {str} -- the file location for the input image.
+            out_path {str} -- the desired location for the output image.
+            width {int} -- The pixel width value. Default=None.
+        Returns:
+            str -- the file path to the output image.
+        """
+        img = Image.open(img_path)
+
+        if width is not None:
+            ratio = width/float(img.size[0])
+            height = int(ratio*float(img.size[1]))
+            img = img.resize((width, height), Image.NEAREST)
+
+        if text is not None and author is not None:
+            draw = ImageDraw.Draw(img)
+            font = ImageFont.truetype(font='./_data/fonts/LilitaOne-Regular.ttf', size=20)
+            draw.text((randint(0,width), randint(0,height)), f'"{text}" - {author}', font=font, fill='white')
+        
+        out_path = self.output_dir + '/img' + str(randint(0, 1000000000)) + '.jpg'
+        img.save(out_path)
+        return out_path
--- src/run.py
+++ src/run.py
@@ -1,5 +1,6 @@
 
 from QuoteEngine import Ingestor
+from MemeEngine import MemeGenerator
 
 
 
@@ -7,3 +8,13 @@ print(Ingestor.parse('./_data/SimpleLines/SimpleLines.docx'))
 print(Ingestor.parse('./_data/SimpleLines/SimpleLines.csv'))
 print(Ingestor.parse('./_data/SimpleLines/SimpleLines.pdf'))
 print(Ingestor.parse('./_data/SimpleLines/SimpleLines.txt'))
+
+
+
+
+
+mg = MemeGenerator("tmp")
+print(mg.make_meme('_data/photos/dog/xander_2.jpg',
+                        'woof!',
+                        'Mamma',
+                        width=500))
